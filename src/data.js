63f44833e19fee3e4cd8cc96f15c1bd2c83dd3dc
Quyet <phamxuanquyet91@gmail.com>
2022-03-23 01:00:04 +0700

    ecommerce app material ui and styled component react

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/564x/74/20/f6/7420f6358394b2836b433e85862bb2e6.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/564x/05/ae/79/05ae79d5a7095b976e73db4ac296275c.jpg",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/564x/e5/2f/7d/e52f7d23ccb09f2b531d82027c85518f.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
